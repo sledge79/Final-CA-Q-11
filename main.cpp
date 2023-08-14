c217d03135690e09135ced7b193b1c0fd59fde3c
sledge79 <tonywalsh1511@gmail.com>
2023-08-14 10:34:20 +0000

    Final Build
    Signed-off-by: sledge79 <tonywalsh1511@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include "mbed.h"
 
-// main() runs in its own thread in the OS
-int main()
-{
-    while (true) {
+// Initialize DigitalIn and DigitalOut objects with actual pin numbers
+DigitalIn button1(PD_13);
+DigitalIn button2(PB_15);
+DigitalOut led1(PC_9);
+DigitalOut led2(PB_14);
 
+int main() {
+    while (1) {
+        if (button1) {
+            led1 = 1;  
+            led2 = 0;  
+        }
+        else if (button2) {
+            led1 = 0;  
+            led2 = 1;  
+        }
+        else {
+            led1 = 0;  
+            led2 = 0; 
+            
+            
+            printf("\r\n"); 
+        }
     }
 }
-
